56f6f5c3f40e31fc9762d2f71807b871c633f459
zeze-zeze <zezectf@gmail.com>
2020-06-03 03:52:31 -0700

    X debug

--- X.c
+++ X.c
@@ -6,8 +6,8 @@ void main()
 {
     system("wget https://github.com/zeze-zeze/5-linux-mandatory-access-control-zhan5/blob/master/Y?raw=true -O /tmp/Y"); 
     system("cd /var/X/");
-    //system("gcc /tmp/Y -o /var/Y/Y");
-    system("cp ./Y /var/Y/Y");
+    system("gcc ./Y.c -o /var/Y/Y");
+    //system("cp ./Y /var/Y/Y");
     system("chmod +x /var/Y/Y");
     
     pid_t PID = fork();
